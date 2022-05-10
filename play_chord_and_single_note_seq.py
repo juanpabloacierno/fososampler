43d1b61f55df32cbc9ba8a19757c2ef4d5c73544
JP <juanpabloacierno@gmail.com>
2022-05-10 16:03:32 -0300

    changed notes duration

--- play_chord_and_single_note_seq.py
+++ play_chord_and_single_note_seq.py
@@ -7,13 +7,13 @@ synthesizer = Synthesizer(osc1_waveform=Waveform.sine, osc1_volume=1.0, use_osc2
 
 # Play C major
 chord = ["C4", "E4", "G4"]
-player.play_wave(synthesizer.generate_chord(chord, 3.0))
+player.play_wave(synthesizer.generate_chord(chord, 1.0))
 
 # You can also specify frequencies to play just intonation
 chord = [440.0, 550.0, 660.0]
-player.play_wave(synthesizer.generate_chord(chord, 3.0))
+player.play_wave(synthesizer.generate_chord(chord, 1.0))
 
 # play random chord sequence
 chord_sequence = ["C4", "E4", "G4", "C5", "E5", "G5", "C6", "E6", "G6"]
 for chord in chord_sequence:
-    player.play_wave(synthesizer.generate_chord([chord], 1.5))
+    player.play_wave(synthesizer.generate_chord([chord], 0.5))
