7ed70ec4151aac1b154686abdd8293e3f91046eb
Ezequiel Abregu <ezequielabregu@Ezequiels-MacBook-Air.local>
2022-05-19 10:10:28 -0300

    Cambios 19.05.2022 10:10 hs)

--- simple_sampler_GUI.py
+++ simple_sampler_GUI.py
@@ -1,5 +1,4 @@
 from tkinter import *
-import mido
 import pygame
 
 i = 1   # init
@@ -8,39 +7,19 @@ root = Tk()
 root.title('fososampler')
 root.geometry("500x400")
 
-pygame.init()   #init pygame
-pygame.mixer.init(frequency=44100, size= -16, channels=2, buffer=128)     # init mixer
+pygame.init()   # init pygame
+pygame.mixer.init(frequency=44100, size=-16, channels=2, buffer=128)     # init mixer
 ch1 = pygame.mixer.Channel(0)  # declare channels
 ch2 = pygame.mixer.Channel(1)
 
 kick = pygame.mixer.Sound('2.wav')  # sound bank
 ton = pygame.mixer.Sound('crovo.wav')
 
-port = mido.get_input_names()   # MIDI port names
-print(port)
-inport = mido.open_input()    # listen port
-
 def play1():
     ch1.play(ton)
-title = Label(root, text="fososampler", bd=9, relief=GROOVE,
-                font=("times new roman", 50, "bold"), bg="white", fg="green")
-title.pack(side=TOP, fill=X)
+    title = Label(root, text="fososampler", bd=9, relief=GROOVE, font=("Calibri",50,"bold"), bg="white", fg="green")
+    title.pack(side=TOP, fill=X)
 
-play_button = Button(root, text="crovo", font=("Helvetica", 32), command=play1)
-play_button.pack(pady=20)
-root.mainloop()
-
-while i == 1:
-    msg = inport.receive()  # create input msg
-    if msg.note == 60 and msg.velocity != 0:
-        ch1.play(kick)  # play ch 1
-        print(f"note {msg.note} dale Cro KICK!")
-
-    if msg.note == 62 and msg.velocity != 0:
-        ch2.play(ton)
-        print(f"note {msg.note} dale Cro TOM!")
-
-
-
-
-
+    play_button = Button(root, text="crovo", font=("Calibri", 32), command=play1)
+    play_button.pack(pady=20)
+    root.mainloop()
